51bffd1c48a5e9097958b4f477d344d39a4db584
Cosmo Alan Pires Lira <105260581+cosmoalanpireslira@users.noreply.github.com>
2024-03-19 03:44:29 -0300

    Update 1. (1013) The Greatest.php

--- atividade II/1. (1013) The Greatest.php
+++ atividade II/1. (1013) The Greatest.php
@@ -7,9 +7,11 @@
     echo "$maior eh o maior\n";
 ?>
 
+[Plano de ensino](https://drive.google.com/file/d/1eC800mzEuvlNBUsh4Qh76_ds1xZXo6o3/view?usp=drive_link)
 <html>
 <body>
 # <h2> Resolução com explicação no Replit: </h2>
 [URI 1013 php-replit](https://replit.com/@Cosmo-AlanAlan/1-1013-The-Greatest) 
+    [Plano de ensino](https://drive.google.com/file/d/1eC800mzEuvlNBUsh4Qh76_ds1xZXo6o3/view?usp=drive_link)
 </body>
 </html>
