9969fd8d68cd9d0e14c586c511b988d9ebe1793b
Cosmo Alan Pires Lira <105260581+cosmoalanpireslira@users.noreply.github.com>
2024-03-19 03:46:26 -0300

    Update 1. (1013) The Greatest.php

--- atividade II/1. (1013) The Greatest.php
+++ atividade II/1. (1013) The Greatest.php
@@ -6,28 +6,3 @@
     
     echo "$maior eh o maior\n";
 ?>
-
-[Plano de ensino](https://drive.google.com/file/d/1eC800mzEuvlNBUsh4Qh76_ds1xZXo6o3/view?usp=drive_link)
-<html>
-<body>
-# <h2> Resolução com explicação no Replit: </h2>
-[URI 1013 php-replit](https://replit.com/@Cosmo-AlanAlan/1-1013-The-Greatest) 
-    [Plano de ensino](https://drive.google.com/file/d/1eC800mzEuvlNBUsh4Qh76_ds1xZXo6o3/view?usp=drive_link)
-</body>
-</html>
-
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
-
-  <!--
-  This script places a badge on your repl's full-browser view back to your repl's cover
-  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-  teal, blue, blurple, magenta, pink!
-  -->
-  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
-  </body>
-</html>
