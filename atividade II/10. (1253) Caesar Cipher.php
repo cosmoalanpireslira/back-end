d53f0dfb9ba375a93bcd3b1d0d2c5f1fc01936c5
Cosmo Alan Pires Lira <105260581+cosmoalanpireslira@users.noreply.github.com>
2024-03-19 22:59:55 -0300

    Create 10. (1253) Caesar Cipher.php

---
+++ atividade II/10. (1253) Caesar Cipher.php
@@ -0,0 +1,33 @@
+<?php
+
+$num_testes = intval(fgets(STDIN));
+
+
+for ($i = 0; $i < $num_testes; $i++) {
+
+  $string = trim(fgets(STDIN));
+
+  $posicoes = intval(fgets(STDIN));
+
+
+  $cifrada = "";
+
+  for ($j = 0; $j < strlen($string); $j++) {
+
+    $ascii = ord($string[$j]);
+
+    $novo_ascii = $ascii - $posicoes;
+
+    if ($novo_ascii < 65) {
+
+      $novo_ascii += 26;
+    }
+
+
+    $cifrada .= chr($novo_ascii);
+  }
+
+  echo $cifrada . "\n";
+}
+
+?>
